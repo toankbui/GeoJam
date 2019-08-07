cc31134f2b14259e282fbfc891f2e154a276dc30
Toan K Bui <32405364+toankbui@users.noreply.github.com>
2019-08-06 19:59:59 -0700

    Initial Commit

---
+++ GeoJam/GeoJam/AppDelegate.swift
@@ -0,0 +1,133 @@
+//
+//  AppDelegate.swift
+//  GeoJam
+//
+//  Created by Scott Beinlich on 8/5/19.
+//  Copyright © 2019 Scott Beinlich. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, SPTSessionManagerDelegate{
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:                 [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let requestedScopes: SPTScope = [.appRemoteControl]
+        self.sessionManager.initiateSession(with: requestedScopes, options: .default)
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]      = [:]) -> Bool {
+        self.sessionManager.application(app, open: url, options: options)
+        return true
+    }
+    
+    //MARK: Spotify Auth
+     
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        print("success", session)
+    }
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        print("fail", error)
+    }
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        print("renewed", session)
+    }
+
+    let SpotifyClientID = "0cae0a79efd74936b4e5bc3c08ad969f"
+    let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
+
+    lazy var configuration = SPTConfiguration(
+    clientID: SpotifyClientID,
+    redirectURL: SpotifyRedirectURL
+    )
+
+    lazy var sessionManager: SPTSessionManager = {
+    if let tokenSwapURL = URL(string: "https://geojams.herokuapp.com/api/token"),
+      let tokenRefreshURL = URL(string: "https://geojams.herokuapp.com/api/refresh_token") {
+     self.configuration.tokenSwapURL = tokenSwapURL
+     self.configuration.tokenRefreshURL = tokenRefreshURL
+     self.configuration.playURI = ""
+    }
+    let manager = SPTSessionManager(configuration: self.configuration, delegate: self)
+    return manager
+    }()
+     
+    
+    // MARK: Application State
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Saves changes in the application's managed object context before the application terminates.
+    self.saveContext()
+    }
+    
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+    /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "GeoJam")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+    if let error = error as NSError? {
+    // Replace this implementation with code to handle the error appropriately.
+    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+    
+    /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+    fatalError("Unresolved error \(error), \(error.userInfo)")
+    }
+})
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+    do {
+        try context.save()
+    } catch {
+    // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+    let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+    }
+    }
+
+}
---
+++ GeoJam/GeoJams-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  GeoJams-Bridging-Header.h
+//  GeoJam
+//
+//  Created by Toan Bui on 8/6/19.
+//  Copyright © 2019 Toan Bui and Scott Beinlich. All rights reserved.
+//
+#import <SpotifyiOS/SpotifyiOS.h>
